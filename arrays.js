57eefdfc976aa0266ca1fe51be5b3cad199ff029
elvshnkv <elvshnkv@gmail.com>
2023-10-18 23:10:07 +0200

    fibonacci task

--- arrays.js
+++ arrays.js
@@ -1,10 +1,6 @@
 let arr  = [7, 1, 3, 55, 7, 7 , 5, 7, 34, 7, 9.45, 22];
 let arr2  = ['Настя','Ира','Катя','Саша','Катя','Настя'];
-let reversedArr
 let i;
-let identifier;
-let positionMatch;
-let valueMatch;
 
 console.log('Начальный массив чисел ' + arr)
 console.log('Начальный массив строк ' + arr2)
@@ -38,7 +34,7 @@ sortDESC(arr);
 
 //поменять массив в обратном порядке
 function reverseArr(arr){
-    reversedArr = arr.reverse();
+    let reversedArr = arr.reverse();
     console.log('Массив в обратном порядке ' + reversedArr)
 }
 reverseArr(arr);
@@ -47,10 +43,10 @@ reverseArr(arr);
 /*даны 2 4-х значных числа с неповторяющимися цифрами,
 надо определить сколько цифр в этих числах совпадают по значению и позиции и сколько только по значению (3487 и 3794 ---> 1 и 2 )*/
 function checkPositionAndValueMatch(number1, number2){
+    let positionMatch = 0;
+    let valueMatch = 0;
     number1 = number1.toString();
     number2 = number2.toString();
-    positionMatch = 0;
-    valueMatch = 0;
 
     for (i=0; i < number1.length; i++){
         if(number1[i] === number2[i]){
@@ -77,13 +73,29 @@ function removeDuplicates(arr){
             arr2.push(arr[i]);
         }
     }
-    console.log('Повторяющиеся и пустые значения удалены: ' + arr2);
+    console.log('Повторяющиеся значения удалены: ' + arr2);
 }
+removeDuplicates(arr);
 removeDuplicates(arr2);
 
 
 //записать в массив ряд фибоначчи начиная с N члена с длинной массива M
+function fibonacciNM(n, m) {
+    const fibonacciStart = [0, 1];
+    function generateFibonacci() {
+        const nextFibonacci = fibonacciStart[fibonacciStart.length - 1] + fibonacciStart[fibonacciStart.length - 2];
+        fibonacciStart.push(nextFibonacci);
 
-function fibonacci(n,m){
+        if (fibonacciStart.length < n + m) {
+            generateFibonacci();
+        }
+    }
 
+    if (n > 1) {
+        generateFibonacci();
+    }
+
+    return fibonacciStart.slice(n - 1, n - 1 + m);
 }
+
+console.log(fibonacciNM(5, 5));
